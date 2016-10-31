4f26afa99b63f913b690629b67769908f00768d5
DonMills <dmills@singlestoneconsulting.com>
2016-10-31 16:46:26 -0400

    Update kmsencrypt.rb

--- kmsencrypt.rb
+++ kmsencrypt.rb
@@ -2,7 +2,7 @@ class Kmsencrypt < Formula
   desc "tool to perform envelope encryption with KMS keys"
   homepage "https://github.com/DonMills/kmsencrypt"
   url "https://github.com/DonMills/kmsencrypt/releases/download/v1.0rc/kmsencrypt.macos64.tar.gz"
-  sha256 "f12392ce0f13beec118ea7d6543471bb501eabb4ac6c4804e3efd93460cab13d"
+  sha256 "b4a2c5cc5639580ce507330c5ad2c3636b75bc7d9e15de431ff340b044e21db9"
 
   bottle :unneeded
 
